5ad3d36211ba6c8bcf8cbde737c8956fea835d81
JohannesSchroth <johannesschroth11@gmail.com>
2020-03-09 17:15:27 +0000

    commit

--- R/app_server.R
+++ R/app_server.R
@@ -1,10 +1,437 @@
-#' The application server-side
-#' 
-#' @param input,output,session Internal parameters for {shiny}. 
-#'     DO NOT REMOVE.
-#' @import shiny
-#' @noRd
-app_server <- function( input, output, session ) {
-  # List the first level callModules here
+#' Application Server
 
+packages_1 <- c('gridExtra','grid', 'ggplot2', 'umap', 'dplyr', 'reshape', 'RColorBrewer', 'cytofkit',
+                'FlowSOM', 'Rtsne', 'ggdendro', 'plotly', 'shiny', 'plotly', 'shinycssloaders', 'monocle',
+                'shinydashboard', 'DT', 'flowCore', 'openCyto', 'ggsci', 'rhandsontable', 'shinyBS')
+lapply(packages_1,library, character.only=TRUE)
+
+
+app_server <- function(input, output, session) {
+  
+  observeEvent(input$upload_data, {
+    
+    rv$raw_data <- flowCore::read.flowSet(files = input$file$datapath)
+    
+    vars <- colnames(rv$raw_data)[-grep('SSC|FSC|Time|Sample|Group', colnames(rv$raw_data))]
+    
+    if (input$logicletransform) {
+      
+      for (i in 1:length(rv$raw_data)) {
+        rv$raw_data[[i]] <- transform(rv$raw_data[[i]], flowCore::estimateLogicle(rv$raw_data[[i]], vars))
+      }
+      
+    } else {
+      NULL
+    }
+    
+    rv$raw_data <- flowSet(rv$raw_data[[1]])
+    
+    rv$raw_data <- as.data.frame(flowCore::exprs(rv$raw_data[[1]]))
+    
+    if (any(colnames(rv$raw_data) == 'SampleID') == TRUE) {
+      
+      dens <- density(rv$raw_data$SampleID, n = length(rv$raw_data$SampleID))
+      samples <- pastecs::turnpoints(ts(dens$y))$pits
+      
+      for (i in 0:length(dens$x[samples]-1)) {
+        rv$raw_data[rv$raw_data$SampleID > 
+                      if (i == 0) {1} else {
+                        dens$x[samples][i]},'Sample'] <- i+1
+      }
+      
+      
+      edit_sample_names_table <- data.frame(Sample = as.character(unique(rv$raw_data$Sample)),
+                                            Name = unique(rv$raw_data$Sample))
+      
+      output$sample_names <- renderRHandsontable(
+        rhandsontable(edit_sample_names_table) %>%
+          hot_col('Name', strict = F)
+      )
+      
+      
+    }
+    
+    edit_colnames_table <- data.frame(
+      Current = colnames(rv$raw_data),
+      New = colnames(rv$raw_data),
+      Include = TRUE,
+      stringsAsFactors = FALSE
+    )
+    
+    output$edit_colnames <- renderRHandsontable(
+      rhandsontable(edit_colnames_table) %>%
+        hot_col('Include', 'checkbox')
+    )
+    
+    
+  })
+  
+  
+  observeEvent(input$upload_data, {
+    
+    showModal(modalDialog(
+      tagList(
+        rHandsontableOutput('edit_colnames',width = '600px')
+      ), 
+      title='Specify Column Names:',
+      footer = tagList(actionButton('submit_colnames', 'Submit'),
+                       modalButton("Cancel")
+      )
+    )
+    )
+  })
+  
+  
+  observeEvent(input$submit_colnames, {
+    
+    
+    removeModal()
+    showModal(modalDialog(
+      tagList(
+        rHandsontableOutput('sample_names',width = '600px')
+      ), 
+      title='Specify Sample Names:',
+      footer = tagList(actionButton('submit_samplenames', 'Submit'),
+                       modalButton("Cancel")
+      )
+    )
+    )
+    removeModal()
+    
+    new_colnames <- as.data.frame(hot_to_r(input$edit_colnames))
+    
+    colnames(rv$raw_data) <- new_colnames[,2]
+    rv$raw_data <- rv$raw_data[,as.logical(as.vector(new_colnames[,3]))]
+    
+    updateSelectInput(session = session, inputId = 'variables_pca', label = paste("Variables"), choices = colnames(rv$raw_data))
+    updateSelectInput(session = session, inputId = 'variables_tsne', label = paste("Variables"), choices = colnames(rv$raw_data))
+    updateSelectInput(session = session, inputId = 'variables_umap', label = paste("Variables"), choices = colnames(rv$raw_data))
+    
+    output$summary_table <- DT::renderDataTable(DT::datatable(rv$raw_data, colnames = colnames(rv$raw_data)) %>% 
+                                                  formatRound(columns = colnames(rv$raw_data)))
+    
+    output$marker_expression <- renderPlot({
+      
+      ggplot(melt(rv$raw_data, variable_name = 'Marker'), aes(x = value, fill = Marker)) +
+        facet_wrap(~Marker, ncol = ceiling(length(rv$raw_data)/4), scales = 'free') +
+        geom_density(alpha = 0.4) +
+        theme_classic() +
+        xlab('MFI') +
+        ylab('Density')
+      
+      
+      
+    })
+    
+    updateSelectInput(session = session,inputId = 'colour_col1', label = 'Colour by:', choices = colnames(rv$raw_data))
+    updateSelectInput(session = session,inputId = 'colour_col2', label = 'Colour by:', choices = colnames(rv$raw_data))
+    updateSelectInput(session = session,inputId = 'colour_col3', label = 'Colour by:', choices = colnames(rv$raw_data))
+    
+    
+    showModal(
+      modalDialog(
+        title = 'Downsampling Data',
+        tagList(
+          
+          sliderInput('n_downsample', 'N', 0, nrow(rv$raw_data), nrow(rv$raw_data))
+          
+        ),
+        
+        footer = tagList(actionButton('submit_downsample', 'Submit'),
+                         modalButton("Cancel"))
+      )
+    )
+  })
+  
+  
+  observeEvent(input$submit_samplenames, {
+    
+    removeModal()
+    
+    new_sample_names <- as.data.frame(hot_to_r(input$sample_names))
+    
+    for (i in 1:length(new_sample_names[,1])) {
+      
+      rv$raw_data[as.character(rv$raw_data$Sample) == new_sample_names[i,1], 'Group'] <- as.character(new_sample_names[i,2])
+      
+    }
+    
+    
+    
+  })
+  
+  
+  observeEvent(input$submit_downsample, {
+    
+    rv$raw_data <- rv$raw_data[sample(nrow(rv$raw_data), input$n_downsample),]
+    
+    removeModal()
+    
+  })
+  
+  
+  normalise <- function(data) {
+    
+    dat <- c()
+    
+    q <- quantile(data, c(0.01, 0.99))
+    
+    for (j in 1:length(data)) {
+      dat[j] <- (data[j] - q[1])/(q[2]-q[1])
+    }
+    dat[dat < 0] <- 0
+    dat[dat > 1] <- 1
+    
+    return(as.numeric(dat))
+    
+  }
+  
+  
+  
+  
+  ##Dim Reduction Methods----
+  
+  observeEvent(input$submit_variables_pca, {
+    
+    pca <- prcomp(as.matrix(rv$raw_data[ ,input$variables_pca]), center = TRUE, scale. = TRUE)
+    rv$pca <- cbind(rv$raw_data, as.data.frame(pca$x[,1:3]))
+    
+    if (input$dimension_num_pca == '2D') { 
+      
+      output$pca_plot <- renderPlotly({
+        
+        plot_ly(data = rv$pca, x = rv$pca$PC1, y = rv$pca$PC2,
+                marker = list(color = ~rv$raw_data[,input$colour_col1], colorscale = c('#FFE1A1', '#683531'))) %>%
+          add_markers()
+      })
+      
+    } else if (input$dimension_num_pca == '3D') {
+      output$pca_plot <- renderPlotly({
+        
+        plot_ly(data = rv$pca, x = rv$pca$PC1, y = rv$pca$PC2, z = rv$pca$PC3,
+                marker = list(color = ~rv$raw_data[,input$colour_col1], colorscale = c('#FFE1A1', '#683531'))) %>%
+          add_markers()
+      })
+      
+    }
+    
+    if(any(input$clustering_type == 'Rphenograph') == TRUE) {
+      
+      phenograph(variables = input$variables_pca, K = 30)
+      
+    } else {
+      NULL
+    }
+  })
+  
+  
+  
+  observeEvent(input$submit_variables_tsne, {
+    
+    set.seed(123)
+    
+    if (input$dimension_num_tsne == '2D') {
+      
+      tsne <- Rtsne::Rtsne(X = rv$raw_data[,input$variables_tsne, drop = FALSE], dims = 2, verbose = T,
+                           perplexity = input$perplexity_tsne, theta = input$theta_tsne, max_iter = input$max_iter_tsne)
+      colnames(tsne$Y) <- c('tSNE1', 'tSNE2')
+      rv$tsne <- as.data.frame(cbind(rv$raw_data, tsne$Y))
+      
+      output$tSNE_plot <- renderPlotly({
+        
+        plot_ly(data = rv$tsne, x = rv$tsne$tSNE1, y = rv$tsne$tSNE2) %>%
+          add_markers(color = ~rv$raw_data[,input$colour_col2]) %>%
+          layout(dragmode = 'lasso') %>%
+          layout(legend = list(title=list(text=''), orientation = 'h'))
+        
+        
+      })
+      
+    } else if (input$dimension_num_tsne == '3D') {
+      
+      tsne <- Rtsne::Rtsne(X = rv$raw_data[,input$variables_tsne, drop = FALSE], dims = 3, verbose = T,
+                           perplexity = input$perplexity_tsne, theta = input$theta_tsne, max_iter = input$max_iter_tsne)
+      colnames(tsne$Y) <- c('tSNE1', 'tSNE2', 'tSNE3')
+      rv$tsne <- as.data.frame(cbind(rv$raw_data[,input$variables_tsne, drop = FALSE], tsne$Y))
+      
+      output$tSNE_plot <- renderPlotly({
+        
+        plot_ly(data = rv$tsne, x = rv$tsne$tSNE1, y = rv$tsne$tSNE2, z = rv$tsne$tSNE3) %>%
+          add_markers(color = ~rv$raw_data[,input$colour_col2]) %>%
+          layout(dragmode = 'lasso', legend = F)
+        
+      })
+      
+    }
+    
+    
+    if (any(grepl('Rphenograph', input$clustering_type_tsne)) == TRUE) {
+      
+      rv$phenograph <- Rphenograph(rv$raw_data[,input$variables_tsne], k = 10)
+      rv$raw_data['Clusters'] <- as.character(rv$phenograph$membership)
+      
+    } else if (any(grepl('ClusterX', input$clustering_type_tsne)) == TRUE) {
+      
+      rv$clusterx <- ClusterX(rv$raw_data[,input$variables, drop = FALSE])
+      rv$raw_data['ClusterX_Clusters'] <- as.character(rv$clusterx$cluster)
+      
+    } else {
+      NULL
+    }
+    
+    updateSelectInput(session = session, inputId = 'colour_col2', label = 'Colour by:', choices = colnames(rv$raw_data))
+    
+    output$tsne_heatmap <- renderPlotly({
+      
+      heat <- as.data.frame(sapply(rv$raw_data[,input$variables_tsne],normalise))
+      heat['Clusters'] <- rv$phenograph$membership
+      
+      heat <- heat %>%
+        group_by(Clusters) %>%
+        summarise_all(median) %>% 
+        as.data.frame() 
+      
+      selected <- sapply(rv$raw_data[,input$variables_tsne],normalise)
+      
+      selected <- selected[which(),] %>%
+        as.data.frame() %>%
+        summarise_all(median) %>%
+        tibble::add_column(Clusters = 'Selected', .before = 1)
+      
+      heat <- rbind(heat, selected)
+      
+      plot_ly(heat, x = colnames(heat)[-1], y = as.character(heat$Clusters),z = as.matrix(heat[,-1]), type = 'heatmap',
+              colors = colorRamp(c("#4575B4",'#FFFFFF','#D73027'))) %>%
+        layout(xaxis = list(type = "category"), 
+               yaxis = list(type = "category"))
+    })
+    
+    
+    output$download_tsne_data <- downloadHandler(
+      
+      filename = function() {
+        paste('tsne data', ".csv", sep = "")
+      },
+      content = function(file) {
+        write.csv(rv$tsne, file, row.names = FALSE)
+      }
+      
+    )
+    
+    
+  })
+  
+  
+  
+  
+  #   }
+  #   
+  #   
+  #   if (any(grepl('ClusterX', input$clustering_type)) == TRUE) {
+  #     
+  #     clusterx <- ClusterX(rv$raw_data[,input$variables, drop = FALSE])
+  #     rv$clusterx <- as.data.frame(cbind(rv$raw_data, 'ClusterX_Clusters' = clusterx$cluster))
+  #     
+  #   }
+  #   
+  #   if (any(grepl('DensVM', input$clustering_type)) == TRUE) {
+  #     
+  #     
+  #     #dens_vm <- DensVM(ydata = as.matrix(data$tSNE1_2D, data$tSNE2_2D), xdata = data[,input$variables])
+  #     #data[,'DensVM_Clusters'] <- dens_vm
+  #     
+  #   }
+  #   
+  #   
+  # })
+  
+  observeEvent(input$submit_variables_umap, {
+    
+    set.seed(123)
+    
+    custom <- umap.defaults
+    custom$verbose <- T
+    custom$n_neighbors <- input$n_neighbours_umap
+    custom$min_dist <- input$min_dist_umap
+    
+    
+    if (input$dimension_num_umap == '2D') {
+      
+      custom$n_components <- 2
+      umap2 <- umap(rv$raw_data[,input$variables_umap, drop = FALSE], config = custom)
+      colnames(umap2$layout) <- c('UMAP1', 'UMAP2')
+      rv$umap2 <- as.data.frame(cbind(umap2$data, umap2$layout))
+      
+      output$umap_plot <- renderPlotly({
+        plot_ly(data = rv$umap2, x = rv$umap2$UMAP1, y = rv$umap2$UMAP2,
+                marker = list(color = ~rv$raw_data[,input$colour_col3], colorscale = c('#FFE1A1', '#683531'))) %>%
+          add_markers()
+      })
+      
+    } else if (input$dimension_num_umap == '3D') {
+      
+      custom$n_components <- 3
+      umap3 <- umap(rv$raw_data[,input$variables_umap, drop = FALSE], config = custom)
+      colnames(umap3$layout) <- c('UMAP1', 'UMAP2', 'UMAP3')
+      rv$umap3 <- as.data.frame(cbind(umap3$data, umap3$layout))
+      output$umap_plot <- renderPlotly({
+        
+        plot_ly(data = rv$umap3, x = rv$umap3$UMAP1, y = rv$umap3$UMAP2, z = rv$umap3$UMAP3,
+                marker = list(color = ~rv$raw_data[,input$colour_col3], colorscale = c('#FFE1A1', '#683531'))) %>%
+          add_markers()
+      })
+    }
+    
+    # if(any(input$clustering_type == 'Rphenograph') == TRUE) {
+    #   phenograph(variables = input$variables_umap, K = 30)
+    # } else {
+    #   NULL
+    # }
+    
+    
+  })
+  
+  
+  
+  # 
+  #Clustering Methods----
+  
+  
+  #if (any(grepl('ClusterX', input$clustering_type)) == TRUE) {
+  #  
+  #  clusterx <- ClusterX(data[,grep('tSNE', colnames(data)), drop = FALSE])
+  #  data <<- as.data.frame(cbind(data, 'ClusterX_Clusters' = clusterx$cluster))
+  #  
+  #}
+  
+  # if (any(grepl('DensVM', input$clustering_type)) == TRUE) {
+  #
+  #
+  #   #dens_vm <- DensVM(ydata = as.matrix(data$tSNE1_2D, data$tSNE2_2D), xdata = data[,input$variables])
+  #   #data[,'DensVM_Clusters'] <- dens_vm
+  #
+  # }
+  
+  #Pseudotime Methods---
+  
+  # if (any(grepl('Monocle3', input$psudotime_type)) == TRUE) {
+  #   
+  #   cell_md <- cbind(seq(1,length(df_all_t[1,]), 1),'1')
+  #   rownames(cell_md) <- cell_md[,1]
+  #   
+  #   cds_all <- new_cell_data_set(as.matrix(t(data)), cell_metadata = cell_md)
+  #   
+  #   cds_all <- preprocess_cds(cds_all, norm_method = 'size_only', num_dim = 2)
+  #   cds_all <- reduce_dimension(cds_all, max_components = 2)
+  #   cds_all <- cluster_cells(cds_all)
+  #   cds_all <- learn_graph(cds_all)
+  #   cds_all <- order_cells(cds_all, root_cells = input$root_cell_cluster)
+  #   
+  #   
+  #   
+  # }
+  
+  
+  
+  
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -1,34 +1,188 @@
-#' The application User-Interface
+#' Application User Interface
 #' 
-#' @param request Internal parameter for `{shiny}`. 
-#'     DO NOT REMOVE.
-#' @import shiny
-#' @noRd
+#' 
+#' 
 app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # List the first level UI elements here 
-    fluidPage(
-      h1("CytoDA")
+    navbarPage(inverse= TRUE, 'CytoDA', 
+               
+               
+               ##Data Formatting Panel----
+               navbarMenu('Data Import & Formatting',
+                          
+                          tabPanel('Summary Table', 
+                                   
+                                   sidebarLayout(
+                                     
+                                     sidebarPanel = sidebarPanel(
+                                       
+                                       #File input
+                                       fileInput("file", label = h3("File input"), multiple = T, accept = '.fcs'),
+                                       
+                                       fluidRow(column(5, verbatimTextOutput("file"))),
+                                       
+                                       checkboxInput('logicletransform', 'Transform data', TRUE),
+                                       
+                                       actionButton('upload_data', 'Upload')),
+                                     
+                                     mainPanel = mainPanel(
+                                       column(DT::dataTableOutput('summary_table'), width = 6)
+                                     )
+                                   )),
+                          
+                          tabPanel('Marker Expression', plotOutput('marker_expression')
+                          )
+               ),
+               
+               
+               ##Dimensionality Reduction Panel---
+               navbarMenu('Dimensionality Reduction',
+                          
+                          tabPanel('PCA',
+                                   
+                                   sidebarLayout(
+                                     
+                                     sidebarPanel = sidebarPanel(
+                                       
+                                       selectInput(inputId = 'variables_pca', label = 'Variables', choices = c(), multiple = T),
+                                       
+                                       selectInput('dimension_num_pca', 'Number of Dimensions', c('2D', '3D')),
+                                       
+                                       selectInput('clustering_type', 'Select Clustering Algorithm', c('Rphenograph', 'ClusterX', 'DensVM'), multiple = T),
+                                       
+                                       actionButton('submit_variables_pca', 'Run'),
+                                       
+                                       width = 3),
+                                     
+                                     mainPanel = mainPanel(
+                                       
+                                       column(6,
+                                              plotlyOutput('pca_plot', height = '500px', width = '500px') %>% 
+                                                shinycssloaders::withSpinner()),
+                                       column(6,
+                                              plotlyOutput('heatmap',height = '500px', width = '500px')),
+                                       hr(),
+                                       fluidRow(
+                                         selectInput('colour_col1', label = 'Colour by:', c()))
+                                     )
+                                   )
+                          ),
+                          
+                          
+                          tabPanel('tSNE', 
+                                   
+                                   sidebarLayout(
+                                     
+                                     sidebarPanel = sidebarPanel(
+                                       
+                                       selectInput(inputId = 'variables_tsne', label = 'Variables', choices = c(), multiple = T),
+                                       
+                                       selectInput('dimension_num_tsne', 'Number of Dimensions', c('2D', '3D')),
+                                       
+                                       sliderInput('perplexity_tsne', 'Perplexity', 0,100,30),
+                                       
+                                       sliderInput('theta_tsne', 'Theta', 0,1,0.5),
+                                       
+                                       sliderInput('max_iter_tsne', 'Number of Iterations', 100,10000,1000),
+                                       
+                                       selectInput('clustering_type_tsne', 'Select Clustering Algorithm', c('Rphenograph', 'ClusterX', 'DensVM'), multiple = T),
+                                       
+                                       actionButton('submit_variables_tsne', 'Run'),
+                                       
+                                       width = 3),
+                                     
+                                     mainPanel = mainPanel(
+                                       
+                                       fluidRow(
+                                         column(6,
+                                                plotlyOutput('tSNE_plot', height = '800px', width = '640px') %>% 
+                                                  shinycssloaders::withSpinner()),
+                                         column(6,
+                                                plotlyOutput('tsne_heatmap',height = '800px', width = '640px')),
+                                         
+                                         hr(),
+                                         fluidRow(
+                                           selectInput('colour_col2', label = 'Colour by:', c()), 
+                                           downloadButton('download_tsne_data', 'Download tSNE Data')
+                                         )
+                                       )
+                                     )
+                                   )),
+                          
+                          tabPanel('UMAP',
+                                   
+                                   sidebarLayout(
+                                     
+                                     sidebarPanel = sidebarPanel(
+                                       
+                                       selectInput(inputId = 'variables_umap', label = 'Variables', choices = c(), multiple = T),
+                                       
+                                       selectInput('dimension_num_umap', 'Number of Dimensions', c('2D', '3D')),
+                                       
+                                       sliderInput('n_neighbours_umap', 'Number of neighbours', min = 2, max = 200, value = 50),
+                                       
+                                       sliderInput('min_dist_umap', 'Minimum Distance', 0.01,0.99,0.5),
+                                       
+                                       selectInput('clustering_type', 'Select Clustering Algorithm', c('Rphenograph', 'ClusterX', 'DensVM'), multiple = T),
+                                       
+                                       actionButton('submit_variables_umap', 'Run'),
+                                       
+                                       width = 3),
+                                     
+                                     mainPanel = mainPanel(
+                                       column(6,
+                                              plotlyOutput('umap_plot', height = '500px', width = '500px') %>% 
+                                                shinycssloaders::withSpinner()),
+                                       # column(6,
+                                       #        plotlyOutput('heatmap',height = '500px', width = '500px')),
+                                       hr(),
+                                       fluidRow(
+                                         selectInput('colour_col3', label = 'Colour by:', c()))
+                                     )
+                                   )
+                          )
+               ),
+               
+               tabPanel('Pseudotime',
+                        fluidPage(
+                          fluidRow(
+                            plotlyOutput('pseudotime_plot', height = '500px', width = '500px') %>%
+                              shinycssloaders::withSpinner()),
+                          hr(),
+                          fluidRow(
+                            
+                          )
+                        )
+               ),
+               
+               tabPanel('Download', 
+                        fluidPage(
+                          bookmarkButton()
+                        )),
+               
+               ##Summary Panel----
+               tabPanel('Summary of Project', 
+                        fluidPage(h1('Cytometry Analysis Tools'),
+                                  br(),
+                                  p(strong('This app was developed to assist in the analysis of single cell data.'))
+                        )
+               )
     )
   )
 }
 
+
+
 #' Add external Resources to the Application
-#' 
-#' This function is internally used to add external 
-#' resources inside the Shiny application. 
-#' 
-#' @import shiny
-#' @importFrom golem add_resource_path activate_js favicon bundle_resources
-#' @noRd
 golem_add_external_resources <- function(){
   
   add_resource_path(
     'www', app_sys('app/www')
   )
- 
+  
   tags$head(
     favicon(),
     bundle_resources(
